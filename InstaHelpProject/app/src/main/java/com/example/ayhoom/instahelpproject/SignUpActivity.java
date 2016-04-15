3c0debd9648d4878258a0ba95881ce1c5e6ade4c
Ayham Masood <sayham@campus.technion.ac.il>
2016-04-15 18:02:37 +0300

    SignIn and SignUp activities were added
    Signed-off-by: Ayham Masood <sayham@campus.technion.ac.il>

--- InstaHelpProject/app/src/main/java/com/example/ayhoom/instahelpproject/SignUpActivity.java
+++ InstaHelpProject/app/src/main/java/com/example/ayhoom/instahelpproject/SignUpActivity.java
@@ -1,27 +1,22 @@
 package com.example.ayhoom.instahelpproject;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
+import android.view.View;
 
-public class LogInActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
-
+        setContentView(R.layout.activity_sign_up);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.login, menu);
-        return true;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
